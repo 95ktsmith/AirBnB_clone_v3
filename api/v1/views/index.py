27ec004ef97233477f716e5cf617b92734279c1c
Kevin Smith <95ktsmith@gmail.com>
2020-09-08 19:40:38 +0000

    Updated to return json content instead of a string.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Index """
 from api.v1.views import app_views
-import json
+from flask import jsonify
 
 
 @app_views.route('/status')
 def status_ok():
     """ Return status ok """
-    return json.dumps({'status': "OK"})
+    return jsonify({'status': "OK"})
