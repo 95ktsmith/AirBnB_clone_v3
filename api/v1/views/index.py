726a204c56fbf204c94b1d5438978eb065587c8b
95ktsmith <95ktsmith@gmail.com>
2020-09-08 22:49:52 +0000

    Added stats method.

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,33 @@
 """ Index """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
 
 
 @app_views.route('/status')
 def status_ok():
     """ Return status ok """
     return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats')
+def stats():
+    """ Returns a dictionary containing the number of objects of each class
+        type in storage.
+    """
+    from models.amenity import Amenity
+    from models.city import City
+    from models.place import Place
+    from models.review import Review
+    from models.state import State
+    from models.user import User
+
+    count_dict = {
+                    "amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)
+    }
+    return jsonify(count_dict)
