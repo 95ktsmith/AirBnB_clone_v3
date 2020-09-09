8d8618fedf79bcaf9ab6ad458ccb785eb99c41a9
Kathleen McKiernan <mckiernan.katie@gmail.com>
2020-09-09 11:15:32 -0400

    add rest of amenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -8,7 +8,6 @@ from flask import Flask, jsonify, request, abort, make_response
 import models
 
 
-
 @app_views.route('/amenities', strict_slashes=False, methods=['GET'])
 def amenity_all():
     """
@@ -20,3 +19,84 @@ def amenity_all():
     return_holder = jsonify(state_holder)
     return return_holder
 
+
+@app_views.route('/amenities/<amenity_id>', strict_slashes=False, methods=['GET'])
+def amenity_one(amenity_id):
+    """
+    State object retrieved with 404 error handling
+    when state_id is not linked to any State object
+    """
+    amenity_one = models.storage.get("Amenity", amenity_id)
+    if amenity_one is None:
+        abort(404)
+    return_holder = jsonify(amenity_one.to_dict())
+    return return_holder
+
+
+@app_views.route('/amenities/<amenity_id>',
+                 strict_slashes=False,
+                 methods=['DELETE'])
+def amenity_delete(amenity_id):
+    """
+    State object deleted with 404 error handling
+    if state_id is not linked to any State object
+    Return: Empty dictionary with status code 200
+    """
+    remove_help = models.storage.get("Amenity", amenity_id)
+    if remove_help is None:
+        abort(404)
+    remove_help.delete()
+    models.storage.save()
+    return_holder = jsonify({})
+    return return_holder
+
+
+@app_views.route('/amenities', strict_slashes=False, methods=['POST'])
+def amenity_create():
+    """
+    State created with specific parameters:
+    - Use Flask's request.get_json to turn HTTP body request to dict
+    - If HTTP body request isn't valid JSON raise error 400 w/ message
+    - If dict doesn't contain key name raise error 400 w/ message
+    Returns: New State with status code 201
+    """
+    request_help = request.get_json()
+    if request_help is None:
+        return_holder = jsonify(error="Not a JSON")
+        return make_response(return_holder, 400)
+    if "name" not in request_help:
+        return_holder = jsonify(error="Missing name")
+        return make_response(return_holder, 400)
+    create_help = models.amenity.Amenity(**request_help)
+    create_help.save()
+    return_holder = jsonify(create_help.to_dict())
+    return make_response(return_holder, 201)
+
+
+@app_views.route('/amenities/<string:amenity_id>',
+                 strict_slashes=False,
+                 methods=['PUT'])
+def amenity_update(amenity_id):
+    """
+    State object updated with specific parameters:
+    - If state_id is not linked to any State object raise 404 error
+    - Use Flask's request.get_json to turn HTTP body request to dict
+    - If HTTP body request isn't valid JSON raise error 400 w/ message
+    - Update State object with all key-value pairs of the dict
+    - Ignore keys: id, created_at and updated_at
+    Return: State object with the status code 200
+    """
+    request_help = request.get_json()
+    if request_help is None:
+        return_holder = jsonify(error="Not a JSON")
+        return make_response(return_holder, 400)
+    ignore_these = ["id", "created_at", "updated_at"]
+    update_help = models.storage.get("Amenity", amenity_id)
+    if update_help is None:
+        abort(404)
+    for k_ey, v_al in request_help.items():
+        if k_ey not in ignore_these:
+            setattr(update_help, k_ey, v_al)
+        update_help.save()
+        return_holder = jsonify(update_help.to_dict())
+        return return_holder
